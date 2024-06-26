2ec84b3b1fad771c82cc02f885e6070192bd3d35
Rafael Henrique de Souza <rafaelhs@ciandt.com>
2024-06-26 11:10:31 -0300

    Create Function Cloud Event

---
+++ src/main/java/org/bullla/poc/sub/HolidayFunctionEvent.java
@@ -0,0 +1,80 @@
+package org.bullla.poc.sub;
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.GrpcTransportChannel;
+import com.google.api.gax.rpc.FixedTransportChannelProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
+import com.google.cloud.functions.CloudEventsFunction;
+import com.google.cloud.pubsub.v1.stub.GrpcSubscriberStub;
+import com.google.cloud.pubsub.v1.stub.SubscriberStub;
+import com.google.cloud.pubsub.v1.stub.SubscriberStubSettings;
+import com.google.gson.Gson;
+import com.google.pubsub.v1.AcknowledgeRequest;
+import com.google.pubsub.v1.ProjectSubscriptionName;
+import com.google.pubsub.v1.PullRequest;
+import com.google.pubsub.v1.PullResponse;
+import com.google.pubsub.v1.ReceivedMessage;
+import io.cloudevents.CloudEvent;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HolidayFunctionEvent implements CloudEventsFunction {
+
+    private static final String EXTERNAL_SERVICE = "http://localhost:3000";
+    private static final String EMULATOR_HOST = "localhost:8085";
+    private static final String PROJECT_ID = "localstack";
+    private static final String SUBSCRIPTION_ID = "subTopic1";
+
+    private static Logger logger = LoggerFactory.getLogger(HolidayFunctionEvent.class);
+
+    @Override
+    public void accept(final CloudEvent cloudEvent) throws Exception {
+        HolidayClient holidayClient = FeignConfig.createClient(HolidayClient.class, EXTERNAL_SERVICE);
+
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(EMULATOR_HOST).usePlaintext().build();
+        TransportChannelProvider channelProvider = FixedTransportChannelProvider.create(GrpcTransportChannel.create(channel));
+        CredentialsProvider credentialsProvider = NoCredentialsProvider.create();
+
+        SubscriberStubSettings subscriberStubSettings = SubscriberStubSettings.newBuilder()
+            .setTransportChannelProvider(channelProvider)
+            .setCredentialsProvider(credentialsProvider)
+            .build();
+
+        try (SubscriberStub subscriber = GrpcSubscriberStub.create(subscriberStubSettings)) {
+            String subscriptionName = ProjectSubscriptionName.format(PROJECT_ID, SUBSCRIPTION_ID);
+            PullRequest pullRequest = PullRequest.newBuilder()
+                .setMaxMessages(1000)
+                .setSubscription(subscriptionName)
+                .setReturnImmediately(true)
+                .build();
+
+            // Use pullCallable().futureCall to asynchronously perform this operation.
+            PullResponse pullResponse = subscriber.pullCallable().call(pullRequest);
+
+            List<String> ackIds = new ArrayList<>();
+            for (ReceivedMessage message : pullResponse.getReceivedMessagesList()) {
+                holidayClient.postHoliday(new Gson().fromJson(message.getMessage().getData().toStringUtf8(), Holiday.class));
+                ackIds.add(message.getAckId());
+            }
+
+            if (!ackIds.isEmpty()) {
+                // Acknowledge received messages.
+                AcknowledgeRequest acknowledgeRequest = AcknowledgeRequest.newBuilder()
+                    .setSubscription(subscriptionName)
+                    .addAllAckIds(ackIds)
+                    .build();
+                // Use acknowledgeCallable().futureCall to asynchronously perform this operation.
+                subscriber.acknowledgeCallable().call(acknowledgeRequest);
+            }
+            logger.info("Messages received: " + pullResponse.getReceivedMessagesList());
+            logger.info("Total messages: " + pullResponse.getReceivedMessagesList().size());
+        }
+    }
+
+}
